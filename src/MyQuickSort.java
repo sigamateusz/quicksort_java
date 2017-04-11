3d505e6436f5b32bc5bb4f1505f7a9a666f58307
cj <sigamateusz@gmail.com>
2017-04-12 01:05:47 +0200

    counts duration time

--- src/MyQuickSort.java
+++ src/MyQuickSort.java
@@ -33,7 +33,15 @@ public class MyQuickSort {
     public static void main (String[] args) {
         String filename = args[0];
         ArrayList<Integer> fromFile = FileOperations.readFromFile(filename);
+        long tStart = System.currentTimeMillis();
+
         ArrayList<Integer> sortedList = quickSort(fromFile);
+
+        long tEnd = System.currentTimeMillis();
+        long tDelta = tEnd - tStart;
+        double elapsedSeconds = tDelta / 1000.0;
+
+        System.out.println("...:::" + elapsedSeconds + "sec:::...");
 
         FileOperations.writeToFile(sortedList, filename);
     }
